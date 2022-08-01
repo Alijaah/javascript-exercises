e189aa937d22400bd0fc29f8ecee9126790e8224
Aidan Dyer <aidan.dyer@live.ca>
2022-07-31 23:09:38 -0400

    complete repeatString excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num > 0) 
+    return string.repeat(num);
+  else if (num < 0)
+  return "ERROR";
+  else 
+    return "";
 };
+
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
