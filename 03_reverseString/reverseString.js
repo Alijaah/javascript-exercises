112999c0bf7bcb49ec2cb0319a8ef1077eb5cc3d
Aidan Dyer <aidan.dyer@live.ca>
2022-08-01 13:48:50 -0400

    finish reverseString lesson

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+  return phrase.split("").reverse().join("");
+}
 
-};
+reverseString('')
 
 // Do not edit below this line
 module.exports = reverseString;
